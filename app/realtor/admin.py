0e4860ce16800a6029bc723f2f770869f9a9364f
Anna Romanovska <sup2a1nn@gmail.com>
2020-05-30 16:19:18 +0300

    New Admin Page

--- app/realtor/admin.py
+++ app/realtor/admin.py
@@ -4,9 +4,11 @@ from .models import Realtor
 
 
 class RealtorAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "phone", "email","birth_date")
+    list_display = ("id", "name", "phone", "email", "birth_date", "hire_date")
     list_display_links = ("id", "name", "phone","email")
     list_per_page = 15
+    search_fields = ("name", "phone")
+    list_filter = ('name', 'hire_date', 'mvr', 'city')
 
 
 admin.site.register(Realtor, RealtorAdmin)
