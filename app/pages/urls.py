d5701dd292b9221f310df6503b7e0f0d6dd2d320
Anna Romanovska <sup2a1nn@gmail.com>
2020-05-31 15:32:45 +0300

    All Pages

--- app/pages/urls.py
+++ app/pages/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('guarantee.html', views.guarantee, name="guarantee"),
     path('we_offer.html', views.we_offer, name="we_offer"),
     path('apartments.html', views.apartments, name="apartments"),
+    path('order.html', views.order, name="order"),
+    path('testimonials.html', views.testimonials, name="testimonials"),
+    path('discounts.html', views.discounts, name="discounts"),
+    path('contact_us.html', views.contact_us, name="contact_us"),
+    path('prices.html', views.prices, name="prices"),
 ]
--- app/pages/views.py
+++ app/pages/views.py
@@ -7,21 +7,46 @@ def index(request):
 
 def about(request):
     data = {"header_h1": "Про <span>Нас</span>",
-            "header_p": "Головна >> Про нас"}
+            "header_p": "<a href='index.html'>Головна</a> >> <a href='about_us.html'>Про нас</a>"}
     return render(request, 'pages/about_us.html', context=data)
 
 
 def guarantee(request):
     data = {"header_h1": "НАШІ <span>ГАРАНТІЇ</span>",
-            "header_p": "Головна >> Гарантії"}
+            "header_p": "<a href='index.html'>Головна</a> >> <a href='guarantee.html'>Гарантії</a>"}
     return render(request, 'pages/guarantee.html', context=data)
 
 def we_offer(request):
     data = {"header_h1": "ЩО <span>ПРОПОНУЄМО</span>",
-            "header_p": "Головна >> Що пропонуємо"}
+            "header_p": "<a href='index.html'>Головна</a> >> <a href='we_offer.html'>Що пропонуємо</a>"}
     return render(request, 'pages/we_offer.html', context=data)
 
 def apartments(request):
-    data = {"header_h1": "НЕРУХОМІСТЬ",
-            "header_p": "Головна >> Квартири"}
+    data = {"header_h1": "КВАРТИРИ <span>ДЛЯ ВАС</span>",
+            "header_p": "<a href='index.html'>Головна</a> >> <a href='apartments.html'>Квартири для вас</a>"}
     return render(request, 'pages/apartments.html', context=data)
+
+def order(request):
+    data = {"header_h1": "ЗАМОВИТИ",
+            "header_p": "<a href='index.html'>Головна</a> >> <a href='order.html'>Замовити</a>"}
+    return render(request, 'pages/order.html', context=data)
+
+def testimonials(request):
+    data = {"header_h1": "ВІДГУКИ",
+            "header_p": "<a href='index.html'>Головна</a> >> <a href='testimonials.html'>Відгуки</a>"}
+    return render(request, 'pages/testimonials.html', context=data)
+
+def discounts(request):
+    data = {"header_h1": "ЗНИЖКИ",
+            "header_p": "<a href='index.html'>Головна</a> >> <a href='discounts.html'>Знижки</a>"}
+    return render(request, 'pages/discounts.html', context=data)
+
+def contact_us(request):
+    data = {"header_h1": "КОНТАКТИ",
+            "header_p": "<a href='index.html'>Головна</a> >> <a href='contact_uss.html'>Контакти</a>"}
+    return render(request, 'pages/contact_us.html', context=data)
+
+def prices(request):
+    data = {"header_h1": "НАШІ <span>ЦІНИ</span>",
+            "header_p": "<a href='index.html'>Головна</a> >> <a href='prices.html'>Наші ціни</a>"}
+    return render(request, 'pages/prices.html', context=data)
